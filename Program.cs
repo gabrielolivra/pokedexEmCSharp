cf5a456b8febcdb635662083951cff40b38b4b6c
gabrielolivraa <gabriel.bernardino@urca.br>
2023-07-18 11:05:07 -0300

    Finalização da api

--- Program.cs
+++ Program.cs
@@ -10,40 +10,50 @@ namespace ConsumindoApiEmCsharp
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("API POKEDEX BUSQUE QUALQUER POKEMON PELO SEU NOME");
-            Console.Write("Digite o nome de um pokemon: ");
-            string nome = Console.ReadLine().ToLower();
-            try
+            bool continuarBusca = true;
+
+           Console.WriteLine("API POKEDEX BUSQUE QUALQUER POKEMON PELO SEU NOME");
+            while (continuarBusca)
             {
-
-                string apiUrl = $"https://pokeapi.co/api/v2/pokemon/{nome}";
-
-                using (HttpClient httpClient = new HttpClient())
+                Console.Write("Digite o nome de um pokemon: ");
+                string nome = Console.ReadLine().ToLower().Trim();
+                try
                 {
+                    
+                    string apiUrl = $"https://pokeapi.co/api/v2/pokemon/{nome}";
 
-                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
-
-                    response.EnsureSuccessStatusCode();
-
-                    string responseBody = await response.Content.ReadAsStringAsync();
-
-                    PokemonData pokemonData = JsonConvert.DeserializeObject<PokemonData>(responseBody);
-
-                    Console.WriteLine($"Nome: {pokemonData.Name}");
-                    Console.WriteLine($"Altura: {pokemonData.Height}");
-                    Console.WriteLine($"Peso: {pokemonData.Weight}");
-                    Console.WriteLine($"Habilidades:");
-                    foreach (var ability in pokemonData.Abilities)
+                    using (HttpClient httpClient = new HttpClient())
                     {
-                        Console.WriteLine($"- {ability.Ability.Name}");
+                        
+                        HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        PokemonData pokemonData = JsonConvert.DeserializeObject<PokemonData>(responseBody);
+
+                        Console.WriteLine($"Nome: {pokemonData.Name}");
+                        Console.WriteLine($"Altura: {pokemonData.Height}");
+                        Console.WriteLine($"Peso: {pokemonData.Weight}");
+                        Console.WriteLine($"Habilidades:");
+                        foreach (var ability in pokemonData.Abilities)
+                        {
+                            Console.WriteLine($"- {ability.Ability.Name}");
+                        }
                     }
+
+                    Console.Write("Deseja buscar outro Pokémon? (S/N): ");
+                    string resposta = Console.ReadLine().Trim();
+                    continuarBusca = resposta.Equals("s", StringComparison.OrdinalIgnoreCase);
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+                    Console.Write("Deseja tentar buscar outro Pokémon? (S/N): ");
+                    string resposta = Console.ReadLine().Trim();
+                    continuarBusca = resposta.Equals("s", StringComparison.OrdinalIgnoreCase);
+                }
             }
         }
     }
-   
+
+
 }
